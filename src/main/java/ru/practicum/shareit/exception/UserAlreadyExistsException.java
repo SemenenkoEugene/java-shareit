c46268ff0b633b0cadfba7f528864a99b3c5d339
SemenenkoEugene <gks-08@mail.ru>
2023-07-07 19:49:44 +0300

    ver.1

--- src/main/java/ru/practicum/shareit/exception/UserAlreadyExistsException.java
+++ src/main/java/ru/practicum/shareit/exception/UserAlreadyExistsException.java
@@ -2,9 +2,6 @@ package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-
 @Slf4j
 public class UserAlreadyExistsException extends RuntimeException {
 
