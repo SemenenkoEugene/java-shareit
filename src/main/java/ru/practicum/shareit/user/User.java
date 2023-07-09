92e6523c2001092490ade491ca90c596085c92fc
SemenenkoEugene <gks-08@mail.ru>
2023-07-09 15:44:11 +0300

    fix: Удалил в моделях аннотации валидации

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -3,17 +3,11 @@ package ru.practicum.shareit.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-
 @Data
 @AllArgsConstructor
 public class User {
 
     private Long id;    //уникальный идентификатор пользователя
-    @NotBlank
     private String name; //имя или логин пользователя
-    @Email
-    @NotBlank
     private String email; // адрес электронной почты
 }
