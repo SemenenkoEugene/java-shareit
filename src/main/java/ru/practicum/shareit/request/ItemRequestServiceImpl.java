3c22af5a0683638f0bb9f669f661eefa775935bc
SemenenkoEugene <gks-08@mail.ru>
2023-08-18 12:33:35 +0300

    fix: исправил по замечаниям

--- src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -13,8 +13,6 @@ import ru.practicum.shareit.request.dto.ItemRequestGetResponseDto;
 import ru.practicum.shareit.user.UserRepository;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
